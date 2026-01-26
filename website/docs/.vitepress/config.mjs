2f30df409f6a7edd2c2d7b0f2d3c25b1b218e2b5
aldiofficial <aeromodac1@gmail.com@gmail.com>
2026-01-26 21:47:51 +0700

    Fix config

--- website/docs/.vitepress/config.mjs
+++ website/docs/.vitepress/config.mjs
@@ -8,19 +8,28 @@ export default defineConfig({
   head: [
     // Favicon
     ['link', { rel: 'icon', href: '/Aldi-Official/logo.png' }],
+    
     // Google Site Verification
-    ['meta', { name: 'google-site-verification', content: '0jovBhkx6wFs5Wi852YS2Z0V8rAK4VXJ8injC0XJ_RI' }]
+    ['meta', { name: 'google-site-verification', content: '0jovBhkx6wFs5Wi852YS2Z0V8rAK4VXJ8injC0XJ_RI' }],
+    
+    // Google AdSense Account
+    ['meta', { name: 'google-adsense-account', content: 'ca-pub-3135834444356256' }],
+
+    // Google AdSense Script
+    ['script', {
+      async: 'true',
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3135834444356256',
+      crossorigin: 'anonymous'
+    }]
   ],
 
   themeConfig: {
     logo: '/Aldi-Official/logo.png',
-
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Telegram', link: 'https://t.me/jurag4n_channel' },
       { text: 'Whatsapp', link: 'https://chat.whatsapp.com/HUYV2mdlKf0IEkX9enm7IK?mode=gi_t' }
     ],
-
     sidebar: [
       {
         text: 'Download SensiX Beta',
@@ -29,16 +38,13 @@ export default defineConfig({
         ]
       }
     ],
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/xmodzid/Aldi-Official' }
     ],
-
     footer: {
       message: 'Official Website',
       copyright: 'Copyright © 2026 Aldi Official'
     },
-
     search: { provider: 'local' }
   }
 })
